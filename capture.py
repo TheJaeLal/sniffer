9ab210ea41a0b993efaeaf653da16113e9313ebf
Jay Lal <jay.lal@somaiya.edu>
2017-08-02 22:48:43 +0530

    Completed exploring ethernet frame

--- capture.py
+++ capture.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import pcapy
-from struct import *
+import struct
 
 def get_mac_addr(bytes_addr):
 	#print(bytes_addr)
@@ -10,18 +10,31 @@ def get_mac_addr(bytes_addr):
 
 def parse_packet(packet):
 	eth_len = 14
-	dest_mac,src_mac,proto = unpack('! 6s 6s H' , packet[:eth_len])	
+	eth_header = packet[:eth_len]
+	eth_data = packet[eth_len:]
+	dest_mac,src_mac,proto = struct.unpack('!6s6s2s' , eth_header)	
 	dest_mac = get_mac_addr(dest_mac)
 	src_mac = get_mac_addr(src_mac)
-	eth_protocol = 
-	print("\n\nDestination Mac:",dest_mac,"\nSource Mac:",src_mac,"\nProtocol:",proto)
-
+	# proto is of the form b'\x08\x00'
+	eth_proto = proto
+	print('\n\n*********Ethernet Frame*********')
+	print("\nSource:",src_mac,"Destination:",dest_mac,"\nProtocol:",eth_proto)
+	print('Data : \n',eth_data)
 
 devs = pcapy.findalldevs()
 #print(devs)
 lan = devs[0]
 wlan = devs[1]
 
+
+'''
+open device
+# Arguments here are:
+#   device
+#   snaplen (maximum number of bytes to capture _per_packet_)
+#   promiscious mode (1 for true)
+#   timeout (in milliseconds)
+'''
 cap = pcapy.open_live(wlan,65536,1,0)
 
 count = 1
